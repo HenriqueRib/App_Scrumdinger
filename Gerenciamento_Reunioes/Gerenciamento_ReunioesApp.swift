315f5fe7c4bec22f3b40777b965af8517ff8503c
HenriqueRib <ribeiro.henriquem@gmail.com>
2022-08-11 06:03:38 -0300

    Initial Commit

---
+++ Gerenciamento_Reunioes/Gerenciamento_ReunioesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gerenciamento_ReunioesApp.swift
+//  Gerenciamento_Reunioes
+//
+//  Created by Henrique Ribeiro on 11/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Gerenciamento_ReunioesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
